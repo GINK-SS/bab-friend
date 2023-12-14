d644e2b9312f8cce341d8cbfea0e98fc8cd167b5
[SW5] 이주현 <leek71358@gmail.com>
2023-12-14 18:41:33 +0900

    style:global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
+import GlobalStyle from "./style/global";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
