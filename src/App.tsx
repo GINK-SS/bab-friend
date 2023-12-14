ccb52a0345204680df749c21566494c09906b5de
[SW5] 이주현 <leek71358@gmail.com>
2023-12-14 18:53:33 +0900

    chore:add Router file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme";
 import GlobalStyle from "./style/global";
+import Router from "./Router";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <RouterProvider router={Router} />
       </ThemeProvider>
     </>
   );
---
+++ src/Router.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const Router = createBrowserRouter([{ path: "", element: <Home /> }]);
+
+export default Router;
