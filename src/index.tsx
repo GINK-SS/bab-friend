b0aa939557e084ef19e56f4e02252b93b538d7bc
Sangmin Lee <gink.ss00@gmail.com>
2023-12-27 13:54:26 +0900

    Feat: recoil 모달 상태관리 변수 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './style/global';
 import { RouterProvider } from 'react-router-dom';
 import theme from './style/theme';
 import Router from './Router';
+import { RecoilRoot } from 'recoil';
+import './assets/fonts/font.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <RouterProvider router={Router} />
     </ThemeProvider>
-  </React.StrictMode>
+  </RecoilRoot>
 );
