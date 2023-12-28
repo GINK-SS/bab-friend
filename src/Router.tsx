026cc50a6c55dcfb1ef89beb2d0a75e85fd27a2c
Sangmin Lee <gink.ss00@gmail.com>
2023-12-29 02:28:13 +0900

    Feat: 토큰과 유저 정보 반환 및 정보 저장 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,12 +3,14 @@ import Home from './pages/Home';
 import MainLayout from './components/MainLayout';
 import CreatePost from './pages/CreatePost';
 import CreatePostContent from './pages/CreatePostContent';
+import Auth from './pages/Auth';
 
 const Router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
       { path: '', element: <Home /> },
+      { path: 'kakao/callback', element: <Auth /> },
       { path: 'createpost', element: <CreatePost /> },
       { path: 'createcontent', element: <CreatePostContent /> },
     ],
