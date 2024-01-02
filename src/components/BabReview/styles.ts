34c0126abcc2f30641c878301b96f6e9c097e5a0
[SW5] 이주현 <leek71358@gmail.com>
2024-01-03 03:23:40 +0900

    design:BabReview 컴포넌트 퍼블리싱

---
+++ src/components/BabReview/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const BabReviewContainer = styled.div`
+  padding: 10px 20px;
+`;
+export const ProfileWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const ProfileImgWrap = styled.div``;
+export const ProfileImg = styled.img`
+  width: 36px;
+  height: 36px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+export const ProfileInfoWrap = styled.div`
+  width: 100%;
+`;
+export const Nickname = styled.p`
+  font-size: 14px;
+  font-family: 'Pretendard-Bold';
+`;
+export const TimeAgo = styled.p`
+  margin-top: -2px;
+  font-family: 'Pretendard-Light';
+  font-size: 12px;
+  opacity: 0.5;
+`;
+export const MoreBtn = styled.img`
+  width: 22px;
+  cursor: pointer;
+`;
+export const ReviewContentWrap = styled.div`
+  padding: 12px 40px;
+  font-size: 12px;
+`;
+export const ReviewContent = styled.p``;
