d02c8e4105c64737eb651a854c088428e451c5f7
[SW5] 이주현 <leek71358@gmail.com>
2024-01-03 03:24:33 +0900

    design:Profile 페이지  퍼블리싱

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,14 +1,31 @@
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../recoil/atoms/user';
+
 import * as S from './styles';
+
 import ProfileInfo from '../../components/ProfileInfo';
+import MannerTemp from '../../components/BabTemp';
+import BabReview from '../../components/BabReview';
+
+import arrow from '../../assets/images/svg/arrow.svg';
 
 const Profile = () => {
   const userInfo = useRecoilValue(userState);
   console.log(userInfo);
   return (
     <>
-      <ProfileInfo></ProfileInfo>
+      <ProfileInfo />
+      <MannerTemp />
+      <S.ReviewHeaderWrap>
+        <S.ReviewHeader>
+          받은 밥 후기<S.ReviewNum>(4)</S.ReviewNum>
+        </S.ReviewHeader>
+        <S.ArrowBtn src={arrow} />
+      </S.ReviewHeaderWrap>
+      <BabReview />
+      <BabReview />
+      <BabReview />
+      <BabReview />
     </>
   );
 };
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,3 +1,24 @@
 import styled from 'styled-components';
 
-export const Profile = styled.div``;
+export const ReviewHeaderWrap = styled.div`
+  margin-top: 40px;
+  margin-bottom: 10px;
+  padding: 0 20px;
+  padding-top: 20px;
+  border-top: 0.1px solid gray;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const ReviewHeader = styled.div`
+  font-family: 'Pretendard-SemiBold';
+  font-size: 18px;
+  cursor: pointer;
+`;
+export const ReviewNum = styled.span`
+  font-size: 12px;
+`;
+export const ArrowBtn = styled.img`
+  width: 20px;
+  cursor: pointer;
+`;
