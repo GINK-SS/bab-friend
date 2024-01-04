a21eee55e0776c96c1653f993e010b9b48d3b830
[SW5] 이주현 <leek71358@gmail.com>
2024-01-04 19:09:20 +0900

    chore:유저정보 get

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,11 @@ import './assets/fonts/font.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const queryClient = new QueryClient();
+
 root.render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../recoil/atoms/user';
+import { fetchUserInfoDetail } from '../../apis/userDetail';
 
 import * as S from './styles';
-
 import ProfileInfo from '../../components/ProfileInfo';
 import MannerTemp from '../../components/BabTemp';
 import BabReview from '../../components/BabReview';
@@ -10,21 +11,24 @@ import BabReview from '../../components/BabReview';
 import arrow from '../../assets/images/svg/arrow.svg';
 
 const Profile = () => {
-  const userInfo = useRecoilValue(userState);
-  console.log(userInfo);
+  const user = useRecoilValue(userState);
+
+  const { data: userInfo } = useQuery({
+    queryKey: ['users'],
+    queryFn: () => fetchUserInfoDetail(user.accessToken),
+  });
+
   return (
     <>
       <ProfileInfo />
       <MannerTemp />
       <S.ReviewHeaderWrap>
         <S.ReviewHeader>
-          받은 밥 후기<S.ReviewNum>(4)</S.ReviewNum>
+          받은 밥 후기
+          <S.ReviewNum>(4)</S.ReviewNum>
         </S.ReviewHeader>
         <S.ArrowBtn src={arrow} />
       </S.ReviewHeaderWrap>
-      <BabReview />
-      <BabReview />
-      <BabReview />
       <BabReview />
     </>
   );
