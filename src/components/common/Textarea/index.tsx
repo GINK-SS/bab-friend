8947ba87e4289e702f87614123b17ed075286c1f
[SW5] 이주현 <leek71358@gmail.com>
2024-01-14 19:18:17 +0900

    feat:input,textarea 공통컴포넌트 생성, 입력값 누락시 에러메시지

---
+++ src/components/common/Textarea/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles';
+
+type TextareaProps = {
+  label: string;
+  placeholder: string;
+  value: string | number;
+  errorMessage?: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+const Input = ({ label, placeholder, value, errorMessage, onChange }: TextareaProps) => {
+  return (
+    <S.TextareaWrap>
+      <S.Label>{label}</S.Label>
+      <S.ElTextarea placeholder={placeholder} value={value} onChange={onChange} />
+      {errorMessage && <S.ErrorMessage>* {errorMessage}</S.ErrorMessage>}
+    </S.TextareaWrap>
+  );
+};
+
+export default Input;
