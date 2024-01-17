4bf369bc6d90e271f1f989c15eeb1e37342553e6
Sangmin Lee <gink.ss00@gmail.com>
2024-01-17 15:16:47 +0900

    Feat: 메인 페이지 글 불러오기 및 컴포넌트 구현

---
+++ src/components/Board/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding: 20px 15px;
+  border-bottom: 5px solid black;
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import { getBoards } from '@_apis/board';
+import Board from '@_components/Board';
+import { BoardInfo } from '@_types/board';
+import { useEffect, useState } from 'react';
+
 const Home = () => {
-  return <>Home</>;
+  const [boards, setBoards] = useState<BoardInfo[]>([]);
+
+  const getBoardData = async () => {
+    const { data } = await getBoards();
+
+    setBoards(data);
+  };
+
+  useEffect(() => {
+    getBoardData();
+  }, []);
+
+  return (
+    <>
+      {boards.map((boardData, index) => (
+        <Board key={index} boardData={boardData} />
+      ))}
+    </>
+  );
 };
 
 export default Home;
