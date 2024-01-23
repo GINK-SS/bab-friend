9ce842438c1aaa5b9386f7f98d9ea1ace3443356
Sangmin Lee <gink.ss00@gmail.com>
2024-01-23 23:53:56 +0900

    Fix: 메인 페이지에 게시물이 없으면 없다는 문구 출력하도록 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { getBoards } from '@_apis/board';
 import Board from '@_components/Board';
 import { BoardInfo } from '@_types/board';
 import { useEffect, useState } from 'react';
+import { IoAddCircle } from 'react-icons/io5';
 
 const Home = () => {
   const [boards, setBoards] = useState<BoardInfo[]>([]);
@@ -18,9 +19,24 @@ const Home = () => {
 
   return (
     <>
-      {boards.map((boardData, index) => (
-        <Board key={index} boardData={boardData} />
-      ))}
+      {boards.length ? (
+        boards.map((boardData, index) => <Board key={index} boardData={boardData} />)
+      ) : (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            alignItems: 'center',
+          }}
+        >
+          <IoAddCircle style={{ marginBottom: '5px', transform: 'rotate(45deg)' }} size={30} />
+          <p>존재하는 게시물이 없습니다. :(</p>
+        </div>
+      )}
     </>
   );
 };
