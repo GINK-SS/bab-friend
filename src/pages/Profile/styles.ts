3302000a8a51dad03445547477605315d7a26d24
Sangmin Lee <gink.ss00@gmail.com>
2024-01-29 22:06:25 +0900

    Design: 받은 밥 후기 헤더 스타일 변경

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,19 +1,17 @@
 import styled from 'styled-components';
 
 export const ReviewHeaderWrap = styled.div`
-  margin-top: 40px;
-  margin-bottom: 10px;
-  padding: 0 20px;
-  padding-top: 20px;
-  border-top: 0.1px solid gray;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-top: 40px;
+  padding: 20px;
+  border-top: 0.1px solid gray;
+  cursor: pointer;
 `;
 export const ReviewHeader = styled.div`
   font-family: 'Pretendard-SemiBold';
   font-size: 18px;
-  cursor: pointer;
 `;
 export const ReviewNum = styled.span`
   margin-left: 5px;
