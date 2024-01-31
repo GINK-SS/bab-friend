8dc6a39941b5735282970e44a95bb97d91fe07a9
Sangmin Lee <gink.ss00@gmail.com>
2024-01-31 15:11:13 +0900

    Fix: 처음 검색 시, 일부분 검색 안되는 문제 수정
    - 리펙토링도 같이 진행

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,55 +14,49 @@ const Home = () => {
   const [isLoadActive, setIsLoadActive] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [isNew, setIsNew] = useState(true);
   const loadTargetRef = useRef<HTMLDivElement>(null);
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         observer.unobserve(entry.target);
-
-        if (isLoadActive) {
-          setPage((prev) => prev + 1);
-        }
+        getBoardData(page + 1);
+        setPage((prev) => prev + 1);
       }
     });
   });
 
-  const getBoardData = async () => {
+  const getBoardData = async (pageNum: number) => {
+    setIsLoading(true);
+
     const {
       data: { boards, last },
-    } = await getBoards({ page: isNew ? 0 : page, search: searchParams.get('search') ?? undefined });
+    } = await getBoards({ page: pageNum, search: searchParams.get('search') ?? undefined });
+    last ? setIsLoadActive(false) : setIsLoadActive(true);
 
-    if (isNew) {
-      setIsLoadActive(true);
-      setIsNew(false);
-      setPage(0);
+    if (pageNum === 0) {
       setBoards(boards);
     } else setBoards((prev) => [...prev, ...boards]);
 
-    if (last) {
-      setIsLoadActive(false);
-    }
+    setIsLoading(false);
   };
 
   useEffect(() => {
-    if (!isNew && page === 0) return;
-
-    setIsLoading(true);
-    getBoardData();
-    setIsLoading(false);
-  }, [page, isNew]);
-
-  useEffect(() => {
-    setIsNew(true);
-  }, [searchParams]);
-
-  useEffect(() => {
-    if (loadTargetRef.current) {
+    if (loadTargetRef.current && isLoadActive) {
       observer.observe(loadTargetRef.current);
     }
+
+    return () => {
+      if (loadTargetRef.current) {
+        observer.unobserve(loadTargetRef.current);
+      }
+    };
   }, [boards]);
+
+  useEffect(() => {
+    setPage(0);
+    getBoardData(0);
+  }, [searchParams]);
 
   return (
     <>
